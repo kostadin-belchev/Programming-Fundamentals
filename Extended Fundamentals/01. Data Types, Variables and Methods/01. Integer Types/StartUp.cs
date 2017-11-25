2e781611fccca580affab0bc5554285f41dc49be
quakeN <quakendev@gmail.com>
2017-11-25 23:38:28 +0200

    Pushing stuff.

---
+++ Extended Fundamentals/01. Data Types, Variables and Methods/01. Integer Types/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.Integer_Types
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numsList = new List<BigInteger>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                numsList.Add(BigInteger.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(" ", numsList));
+        }
+    }
+}
