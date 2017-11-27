14f120ba184f8abd124ceb39aed1d2eb9c61a197
quakeN <quakendev@gmail.com>
2017-11-27 22:13:52 +0200

    Pushing problems.

---
+++ Extended Fundamentals/01. Data Types, Variables and Methods/02. Real Number Types/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.Real_Number_Types
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            decimal countOfDigits = int.Parse(Console.ReadLine());
+            decimal number = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{number}");
+        }
+    }
+}
---
+++ Extended Fundamentals/01. Data Types, Variables and Methods/03. Big Power/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _03.Big_Power
+{
+    using System;
+    using System.Numerics;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int numberToPower = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{BigInteger.Pow(numberToPower, numberToPower)}");
+        }
+    }
+}
